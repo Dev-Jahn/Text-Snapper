dcd534d205c5ae8a7ebce9130539b23ab9bda9a6
Dev-Jahn <ajh508@naver.com>
2017-12-03 15:55:41 +0900

    Commit before merge

--- app/src/main/aidl/kr/ac/ssu/cse/jahn/textsnapper/ocr/IOCRServiceCallback.aidl
+++ app/src/main/aidl/kr/ac/ssu/cse/jahn/textsnapper/ocr/IOCRServiceCallback.aidl
@@ -1,15 +1,13 @@
-// IOCRService.aidl
+// IOCRServiceCallback.aidl
 package kr.ac.ssu.cse.jahn.textsnapper.ocr;
-import android.content.Intent;
-import kr.ac.ssu.cse.jahn.textsnapper.ocr.IOCRServiceCallback;
+import android.os.Message;
 // Declare any non-default types here with import statements
 
-interface IOCRService {
+interface IOCRServiceCallback {
     /**
      * Demonstrates some basic types that you can use as parameters
      * and return values in AIDL.
      */
     void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString);
-    oneway void setCallback(IOCRServiceCallback callback);
-    void startOCR(in Intent intent);
+    void sendResult(in Message msg);
 }
--- app/src/main/java/kr/ac/ssu/cse/jahn/textsnapper/ui/TestActivity.java
+++ app/src/main/java/kr/ac/ssu/cse/jahn/textsnapper/ui/TestActivity.java
@@ -3,17 +3,24 @@ package kr.ac.ssu.cse.jahn.textsnapper.ui;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.googlecode.leptonica.android.Pix;
+import com.googlecode.leptonica.android.WriteFile;
+
 import kr.ac.ssu.cse.jahn.textsnapper.R;
+import kr.ac.ssu.cse.jahn.textsnapper.ocr.IOCRService;
+import kr.ac.ssu.cse.jahn.textsnapper.ocr.IOCRServiceCallback;
 import kr.ac.ssu.cse.jahn.textsnapper.ocr.ImageSource;
+import kr.ac.ssu.cse.jahn.textsnapper.ocr.OCRProcessor;
 import kr.ac.ssu.cse.jahn.textsnapper.ocr.OCRService;
 
 public class TestActivity extends AppCompatActivity
@@ -23,22 +30,64 @@ public class TestActivity extends AppCompatActivity
     protected EditText mText;
     protected Uri mPhotoUri;
     protected ImageSource mImageSource;
+    protected IOCRService mBinder = null;
+    protected Pix mFinalPix;
+    private boolean _binded = false;
 
-    private boolean _Connected=false;
+
     private ServiceConnection mConnection = new ServiceConnection()
     {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service)
         {
-            Log.e(TAG, "onServiceConnected");
-            _Connected=true;
+            Log.e(TAG, "Service Connected");
+            mBinder = IOCRService.Stub.asInterface(service);
+            _binded = true;
+            start();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name)
         {
-            Log.e(TAG, "onServiceDisconnected");
-            _Connected=false;
+            Log.e(TAG, "Service disconnected");
+            mBinder = null;
+            _binded = false;
+        }
+    };
+
+    IOCRServiceCallback mCallback = new IOCRServiceCallback()
+    {
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException
+        {
+
+        }
+
+        @Override
+        public void sendResult(Message msg) throws RemoteException
+        {
+            switch (msg.what)
+            {
+            case OCRProcessor.MESSAGE_FINAL_IMAGE:
+                long nativePix = (long) msg.obj;
+                if (nativePix != 0)
+                {
+                    mFinalPix = new Pix(nativePix);
+                    mImageView.setImageBitmap(WriteFile.writeBitmap(mFinalPix));
+                }
+                break;
+
+            case OCRProcessor.MESSAGE_UTF8_TEXT:
+                mText.setText((String) msg.obj);
+                break;
+
+            }
+        }
+
+        @Override
+        public IBinder asBinder()
+        {
+            return null;
         }
     };
 
@@ -49,26 +98,34 @@ public class TestActivity extends AppCompatActivity
         setContentView(R.layout.activity_test);
 
         mText = (EditText) findViewById(R.id.editText);
-        mImageView = (ImageView)findViewById(R.id.imageView);
+        mImageView = (ImageView) findViewById(R.id.imageView);
 
-        Intent caller = getIntent();
-        mPhotoUri = caller.getData();
-        mImageSource = (ImageSource)caller.getSerializableExtra("imagesource");
 
         Intent intent = new Intent(this, OCRService.class);
         bindService(intent, mConnection, BIND_AUTO_CREATE);
-        if (_Connected)
+    }
+
+    protected void start()
+    {
+        Intent source = getIntent();
+        source.putExtra("lang", "kor");
+        //main action
+        try
         {
-            intent.setDataAndType(mPhotoUri,"image/*");
-            intent.putExtra("imagesource", mImageSource);
-            startService(intent);
+            mBinder.setCallback(mCallback);
+            mBinder.startOCR(source);
+        }
+        catch (RemoteException e)
+        {
+            Log.e(TAG, "RemoteException");
+            e.printStackTrace();
         }
     }
 
     @Override
     protected void onDestroy()
     {
-        unbindService(mConnection);
         super.onDestroy();
+        unbindService(mConnection);
     }
 }
