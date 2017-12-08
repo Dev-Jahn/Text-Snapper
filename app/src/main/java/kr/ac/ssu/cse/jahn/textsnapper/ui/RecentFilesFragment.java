1c9d9500e48e56d4f47f7973e18bb92fc927b9e0
archslaveCW <archslave@naver.com>
2017-12-09 02:30:22 +0900

    #2017. 12. 09. 0232i
    1. File Browser (100%)
    2. Dialog is implemented
    3. I want to rest

--- app/src/main/java/kr/ac/ssu/cse/jahn/textsnapper/ui/RecentFilesFragment.java
+++ app/src/main/java/kr/ac/ssu/cse/jahn/textsnapper/ui/RecentFilesFragment.java
@@ -1,13 +1,30 @@
 package kr.ac.ssu.cse.jahn.textsnapper.ui;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+
+import java.io.File;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import kr.ac.ssu.cse.jahn.textsnapper.R;
+import kr.ac.ssu.cse.jahn.textsnapper.ui.db.FileAdapter;
+import kr.ac.ssu.cse.jahn.textsnapper.ui.db.FileDatabase;
+import kr.ac.ssu.cse.jahn.textsnapper.ui.db.Item;
+import kr.ac.ssu.cse.jahn.textsnapper.util.Utils;
 
 /**
  * Created by ArchSlave on 2017-11-16.
@@ -15,8 +32,136 @@ import kr.ac.ssu.cse.jahn.textsnapper.R;
 
 public class RecentFilesFragment extends Fragment {
 
+    private static FileAdapter adapter;
+    private Context context;
+    private ListView mListView;
+    private static ArrayList<Item> mList;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return (LinearLayout) inflater.inflate(R.layout.fragment_recentfiles, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_recentfiles, container, false);
+        mListView = (ListView) view.findViewById(R.id.listView);
+        mList = new ArrayList<Item>();
+
+        /**
+         * 파일을 클릭했을 때에 대한 처리
+         */
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Item curItem = mList.get(position);
+                /**
+                 * Activity로 넘기기 바랍니다.
+                 */
+            }
+        });
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DEBUG5", "Longclick");
+                final Item curItem = mList.get(position);
+
+                PopupMenu mPopup = new PopupMenu(context, view, Gravity.RIGHT);
+
+                mPopup.getMenuInflater().inflate(R.menu.menu_file_pop_up, mPopup.getMenu());
+
+                mPopup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        FileDatabase database = FileDatabase.getInstance(context);
+                        switch (item.getItemId()) {
+                            case R.id.setFavoriteMenu:
+                                if(FavoriteFragment.isAlreadyAdded(curItem) == false) {
+                                    ContentValues addRowValue = new ContentValues();
+                                    addRowValue.put("filename", curItem.getFileName());
+                                    addRowValue.put("file", curItem.getFilePath());
+                                    addRowValue.put("nickname", "null");
+                                    addRowValue.put("flag", 0);
+                                    database.insert(addRowValue);
+                                    FavoriteFragment.addFavorite(curItem);
+                                }
+                                break;
+
+                            case R.id.deleteMenu:
+                                File delFile = new File(curItem.getFilePath());
+                                delFile.delete();
+                                database.delete("file='"+curItem.getFilePath()+"'", null);
+                                mList.remove(curItem);
+                                adapter.notifyDataSetChanged();
+                                FavoriteFragment.deleteFile(curItem);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
+                mPopup.show();
+
+                // click 이벤트로 안넘어갈 것이기 때문에 true, 변경 금지
+                return true;
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.d("DEBUG8", "ac");
+        super.onActivityCreated(savedInstanceState);
+        context = getActivity().getApplicationContext();
+        updateAdapterList();
+        adapter = new FileAdapter(context, mList);
+        mListView.setAdapter(adapter);
+    }
+
+    public void updateAdapterList() {
+        File curDir = new File(Utils.DATA_PATH);
+        mList.clear();
+        File[] curFiles = curDir.listFiles();
+
+        try {
+            for (File curFile : curFiles) {
+                Log.d("FileIO", curFile.getAbsolutePath());
+                Date lastModDate = new Date(curFile.lastModified());
+                DateFormat formatter = DateFormat.getDateTimeInstance();
+                String modDate = formatter.format(lastModDate);
+
+                if (!curFile.isDirectory()) {
+                    String fileUnit = "Bytes";
+                    long fileSize = curFile.length();
+                    if (fileSize > 1024) {
+                        fileSize /= 1024;
+                        fileUnit = "KB";
+                        if (fileSize > 1024) {
+                            fileSize /= 1024;
+                            fileUnit = "MB";
+                        }
+                    }
+                    mList.add(new Item(curFile.getName(), fileSize + fileUnit, modDate, curFile.getAbsolutePath()));
+                }
+            }
+        } catch (Exception e) {
+            Log.d("EXCEPTION", "Exception : " + e);
+        }
+        Collections.sort(mList);
+    }
+
+    public void onResume() {
+        super.onResume();
+        updateAdapterList();
+        adapter.notifyDataSetChanged();
+    }
+
+
+    /**
+     * 주의! Fragment 상호 교류를 위해 어쩔 수 없이 채택한 코드
+     * Fragment 외 호출 금지
+     */
+    protected static void deleteFile(Item newItem) {
+        mList.remove(newItem);
+        adapter.notifyDataSetChanged();
     }
 }
